d0631748d47a8e764c5d13484fa239f0145a7ac1
JamesHarden24 <410212660@qq.com>
2017-09-07 07:54:12 +0800

    【刷新笔记】

---
+++ iTOP4412/Linux驱动/15_LEDS驱动/leds.c
@@ -0,0 +1,16 @@
+/*******************************************************************************
+
+(a)在Linux申请gpio的头文件 (函数:gpio_request等)
+   -- include/linux/gpio.h
+   
+(b)三星平台的GPIO配置函数头文件
+   -- arch/arm/plat-samsung/include/plat/gpio-cfg.h
+
+(c)三星平台EXYNOS系列平台，GPIO配置参数宏定义头文件
+   -- arch/arm/mach-exynos/include/mach/gpio.h
+   -- GPIO管脚拉高拉低配置参数等等
+(d)三星平台4412平台，GPIO宏定义头文件。已经包含在头文件gpio.h中了
+   -- arch/arm/mach-exynos/include/mach/gpio-exynos4.h
+
+   
+*******************************************************************************/
